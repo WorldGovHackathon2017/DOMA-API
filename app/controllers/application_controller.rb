26ab502e1a4a694a2ddf3964d40968dbda5d5469
Jenna Ritten <jritten@umich.edu>
2017-12-18 12:30:55 -0500

    Configure access restrictions to all routes except login. Need to uncomment before use.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::API
   # protect_from_forgery with: :exception
+  # include ActionController::Cookies
+  # after_action :cors_set_access_control_headers
+
+  # def current_user
+  #   return nil if cookies[:jwt].nil? || cookies[:jwt].empty?
+  #   token = Tokenize.decode(cookies[:jwt])
+  #   User.try(:find_by, {id: token.first['sub']['id']})
+  # end
+
+  # def logged_in?
+  #   !!current_user
+  # end
+
+  # def cors_set_access_control_headers
+  #   headers['Access-Control-Allow-Origin'] = '*'
+  #   headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+  #   headers['Access-Control-Allow-Headers'] = '*'
+  # end
+
+  # add 'before_action :authorize' to controllers
+  # def authorize
+  #   render json: {error: "Please Login", message: "Login or Create an Account"}, status: 403 if !logged_in?
+  # end
 end
