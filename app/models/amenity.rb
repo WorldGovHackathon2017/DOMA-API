ea140c51db9f97ce6efa484de0018c7bd0e5529d
Jenna Ritten <jritten@umich.edu>
2017-12-21 14:02:23 -0500

    Add 'json_with_associations' to models.

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -4,4 +4,12 @@ class Amenity < ApplicationRecord
   has_many :requests, dependent: :destroy
   has_many :refugees, through: :requests
 
+  def json_with_association
+    self.as_json(include: {
+    listings: {include: {host: {}, amenity: {}}},
+    requests: {include: {refugee: {}, amenity: {}}},
+    hosts: {},
+    refugees: {}
+  })
+  end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,10 @@ class Listing < ApplicationRecord
   belongs_to :host
   belongs_to :amenity
   
+  def json_with_associations
+    self.as_json(include: {
+      host: {},
+      amenity: {}
+      })
+  end
 end
