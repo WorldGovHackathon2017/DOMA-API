67c6fba400e2ed4327b1f3c924093a7017b92060
Jenna Ritten <jritten@umich.edu>
2017-12-24 13:37:11 -0500

    Add routes to routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope :api do 
-    
+    resources :hosts
+    resources :listings
+    resources :amenities
+    resources :refugees
+    resources :requests
+    get '/host_profile', to: 'hosts#profile'
+    get '/refugee_profile', to: 'refugees#profile'
+    get '/login', to: 'sessions#login'
+    get '/logout', to: 'sessions#logout'
   end
 end
